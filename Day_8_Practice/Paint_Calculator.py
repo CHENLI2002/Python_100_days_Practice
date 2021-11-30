7f6cb254b1fabe83b58c765d35ca496395241e90
Chen Li <90521572+CHENLI2002@users.noreply.github.com>
2021-11-29 21:58:26 -0600

    Create Paint_Calculator.py

---
+++ Day_8_Practice/Paint_Calculator.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+
+def prime_checker(number):
+    prime_or_not = false
+    
+    for num in range(2, number):
+        if number % num is 0:
+            
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
